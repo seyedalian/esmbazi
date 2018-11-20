18e036762e3da06508bcc2d958dc4234b5a637ec
seyedsajjad <seyedalian10@gmail.com>
2018-11-20 21:38:24 +0330

    On branch master 	modified:   MainActivity.kt : to make ready mainACtivity 	modified:   activity_main.xml : add a simple ui

--- app/src/main/java/ir/sssa/esmbazi/view/MainActivity.kt
+++ app/src/main/java/ir/sssa/esmbazi/view/MainActivity.kt
@@ -2,12 +2,37 @@ package ir.sssa.esmbazi.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import ir.sssa.esmbazi.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val gameWithAndroid :TextView = findViewById(R.id.gameWithAndroid)
+    val ftfGame :TextView = findViewById(R.id.ftfGame)
+
+        gameWithAndroid.setOnClickListener(this)
+        ftfGame.setOnClickListener(this)
+
+
+
+
+    }
+
+    override fun onClick(view: View?) {
+        if (view != null) {
+            if(view.id == R.id.gameWithAndroid){
+
+            }
+            if(view.id == R.id.ftfGame){
+
+            }
+
+        }
     }
 }
