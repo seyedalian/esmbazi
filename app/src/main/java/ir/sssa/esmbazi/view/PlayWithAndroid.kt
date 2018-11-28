5e90ea898bf59b9dca583abef8db1402936f3ca2
seyedsajjad <seyedalian10@gmail.com>
2018-11-28 18:39:40 +0330

    add converter image to drawable

--- app/src/main/java/ir/sssa/esmbazi/view/PlayWithAndroid.kt
+++ app/src/main/java/ir/sssa/esmbazi/view/PlayWithAndroid.kt
@@ -1,13 +1,42 @@
 package ir.sssa.esmbazi.view
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.support.annotation.DrawableRes
+import android.support.constraint.ConstraintLayout
 import ir.sssa.esmbazi.R
+import java.io.ByteArrayOutputStream
 
 class PlayWithAndroid : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_with_android)
+
+    }
+
+    private fun imageInDrawable(@DrawableRes id: Int): Drawable {
+
+
+        var bitmap = BitmapFactory.decodeResource(resources, id)
+        bitmap = Bitmap.createScaledBitmap(bitmap, 300, 700, true)
+
+        val bitmap2: Bitmap
+        val BYTE: ByteArray
+        val bytearrayoutputstream: ByteArrayOutputStream
+        bytearrayoutputstream = ByteArrayOutputStream()
+
+
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytearrayoutputstream)
+        BYTE = bytearrayoutputstream.toByteArray()
+
+        bitmap2 = BitmapFactory.decodeByteArray(BYTE, 0, BYTE.size)
+        return BitmapDrawable(resources, bitmap)
+
+
     }
 }
