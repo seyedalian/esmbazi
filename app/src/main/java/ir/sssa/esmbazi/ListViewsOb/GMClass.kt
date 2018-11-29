ea4dea824e710fd102c9c2acdb098e1ddc17b19f
seyedsajjad <seyedalian10@gmail.com>
2018-11-29 19:55:19 +0330

    add isTrue in constructor

--- app/src/main/java/ir/sssa/esmbazi/ListViewsOb/GMClass.kt
+++ app/src/main/java/ir/sssa/esmbazi/ListViewsOb/GMClass.kt
@@ -1,8 +1,8 @@
 package ir.sssa.esmbazi.ListViewsOb
 
-class GMClass(text:String,who:Boolean){
+class GMClass(text:String,who:Boolean,isTrue:Condition){
     //who ==true main person and who == false main partner
-    var  isTrue:Condition = Condition.UNKNOWN
+    var  isTrue:Condition = isTrue
     val text:String =text
     val who:Boolean =who
 }
