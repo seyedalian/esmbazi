a7a1858af3722c4320457eeda58f760e4a47e13d
unknown <seyedalian10@gmail.com>
2019-01-23 18:38:18 +0330

    edit splash

--- app/src/main/java/sssa/ir/esmbazi/view/Splash.kt
+++ app/src/main/java/sssa/ir/esmbazi/view/Splash.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 import ir.sssa.esmbazi.R
 
 
@@ -12,11 +13,14 @@ class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val iconInSplash:ImageView =findViewById(R.id.iconInSplash)
+        iconInSplash.alpha=0f
 
+        iconInSplash.animate().alpha(1f).rotation(360f).setDuration(2000L)
         Handler().postDelayed({
             startActivity(Intent(this@Splash,MainActivity::class.java))
             finish()
 
-        },1000)
+        },2500L)
     }
 }
